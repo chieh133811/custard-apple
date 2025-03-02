d2ca34b6e44b66da361283142e79c4a9bbb44929
婕予 <qq133811@gmail.com>
2025-03-02 22:47:51 +0800

    [修改]打包

---
+++ public/main/assets/RegisterView-BbsbRRw9.js
@@ -0,0 +1,1 @@
+import{d as e,c as s,a as t,E as a,o}from"./index-CJmEcR26.js";const r={class:"register"},p=e({__name:"RegisterView",setup(c){return(n,_)=>(o(),s("div",r,[t(a)]))}});export{p as default};
